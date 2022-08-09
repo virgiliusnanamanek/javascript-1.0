dece35eedec1ec48409ff759ddba7302bd649cb2
virgiliusnanamanek <vickynana034@gmail.com>
2022-08-09 15:04:34 +0700

    Update coding-challenge-two.js

--- fundamental-two/coding-challenge-two.js
+++ fundamental-two/coding-challenge-two.js
@@ -3,5 +3,13 @@ const calcTip = (bill) =>
 
 const bills = [124, 48, 268];
 
+const tips1 = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const finalValues = [
+  bills[0] + tips1[0],
+  bills[1] + tips1[1],
+  bills[2] + tips1[2],
+];
+
 const tips = bills.map(calcTip);
-console.log(tips);
+console.log(tips, tips1);
