084b113d28a1a8df7533cbe821db787f43a8c6ea
virgiliusnanamanek <vickynana034@gmail.com>
2022-08-09 13:57:24 +0700

    Create values-and-variables.js

---
+++ fundamental-one/values-and-variables.js
@@ -0,0 +1,6 @@
+let country = "Indonesia";
+let continent = "Asia";
+let population = 25000000;
+
+console.log(`${country} is in ${continent}`);
+console.log(`${country} has a population of ${population}`);
