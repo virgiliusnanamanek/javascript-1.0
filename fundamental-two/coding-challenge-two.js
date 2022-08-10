4c6d53d6376bb79e33c105016825622259c37666
virgiliusnanamanek <vickynana034@gmail.com>
2022-08-10 17:12:17 +0700

    Update coding-challenge-two.js

--- fundamental-two/coding-challenge-two.js
+++ fundamental-two/coding-challenge-two.js
@@ -4,12 +4,13 @@ const calcTip = (bill) =>
 const bills = [124, 48, 268];
 
 const tips1 = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-
-const finalValues = [
-  bills[0] + tips1[0],
-  bills[1] + tips1[1],
-  bills[2] + tips1[2],
+const tips2 = [
+  calcTip(bills[0] + tips1[0]),
+  calcTip(bills[1] + tips1[1]),
+  calcTip(bills[2] + tips1[2]),
 ];
 
 const tips = bills.map(calcTip);
-console.log(tips, tips1);
+console.log(tips);
+console.log(tips1);
+console.log(tips2);
